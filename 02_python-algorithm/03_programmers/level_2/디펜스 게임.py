6c99d0296000e4ad6a74e7a14443d7d11e25707d
Kim Yoo <kimyoo04eco@naver.com>
2022-12-14 23:24:56 +0900

    create 디펜스 게임.py

---
+++ 02_python-algorithm/03_programmers/level_2/디펜스 게임.py
@@ -0,0 +1,31 @@
+from heapq import heappush, heappop, heappushpop
+
+def solution(n, k, enemy):
+    max_heap = []
+    round = 0
+    total = 0
+    
+    for enemies in enemy:
+        # 적의 수 모두 더하기
+        total += enemies
+        
+        # 준호의 병사 사용
+        if total <= n:
+            heappush(max_heap, -enemies)
+            round += 1
+            
+        # 무적권 있으면 사용
+        elif k > 0:
+            k -= 1
+            # 최대 힙 사용 및 작은 값 빼기
+            # heappushpop(max_heap, -enemies)
+            heappush(max_heap, -enemies)
+            total += heappop(max_heap) 
+            round += 1
+        
+        else: 
+            break
+    
+    return round
+
+
