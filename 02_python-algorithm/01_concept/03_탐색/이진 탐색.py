ea108514a088882f67ae90c7b0fe1d15e40bf6e9
Kim <58503130+kimyoo04@users.noreply.github.com>
2022-12-20 20:08:17 +0900

    Create 이진 탐색.py

---
+++ 02_python-algorithm/01_concept/03_탐색/이진 탐색.py
@@ -0,0 +1,26 @@
+def binary_search1(array, target, start, end):
+    if start > end:
+        return None
+    mid = (start + end) // 2
+    
+    if array[mid] == target:
+        return mid
+    elif array[mid] > target:
+        return binary_search1(array, target, start, mid - 1)
+    else:
+        return binary_search1(array, target, mid + 1, end)
+
+
+def binary_search2(array, target, start, end):
+    while start <= end:
+        mid = (start + end) // 2
+        if array[mid] == target:
+            return mid
+        elif array[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+    return None
+
+
+# 참고: https://velog.io/@kimdukbae/%EC%9D%B4%EB%B6%84-%ED%83%90%EC%83%89-%EC%9D%B4%EC%A7%84-%ED%83%90%EC%83%89-Binary-Search
