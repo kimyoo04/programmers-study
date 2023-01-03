9c48b5e16aaf527c643a2733b1ec2d9a30027c68
Kim Yoo <kimyoo04eco@naver.com>
2023-01-03 23:40:14 +0900

    Solve 추석 트래픽

--- 02_python-algorithm/03_programmers/level_3/추석 트래픽 - 슬라이딩 윈도우.py
+++ 02_python-algorithm/03_programmers/level_3/추석 트래픽 - 슬라이딩 윈도우.py
@@ -25,28 +25,99 @@
 
 '''
 필요한 핵심
-- 날짜가 넘어가는 경우의 비교 방법
+- 날짜가 넘어가는 경우의 비교 방법 -> datetime 모듈 사용
 - 슬라이딩 윈도우 활용, 각 로그들의 시작 시간과 끝 시간 주변 탐색, 겹치는 최대의 로그 수 반환
 - 작업 시작과 작업 끝 시간의 처리를 어떻게 하는지 파악
 - 날짜 비교, 날짜 연산 방법
 - float의 경우 셋째 자리까지
 '''
 
-def solution(lines):
+# 실패한 사례
+def solution1(lines):
     answer = 0
-    
+
     lines = [x.split(" ")[-2:] for x in lines]
-    
+
     new_lines = []
     for line in lines:
         line = [line[0].split(":"), line[1][:-1]]
-        new_lines.append(line)        
-    
+        new_lines.append(line)
+
     for a_log in new_lines:
         hh, mm, ss = a_log[0]
         hh, mm = int(hh), int(mm)
         ss = float(ss)
         work_time = float(a_log[1])
-        
+
         print(hh, mm, ss, '-',work_time)
     return answer
+
+
+"""
+실패한 이유
+- 데이터 가공에 날짜가 빠져서 정확한 작업의 시작시간을 알 수가 없다.
+- 마구잡이로 시작해서 자료구조를 어떤 것을 쓸지 확정짓지 않았다.
+- 경계값 처리 전략을 정하지 않았다.
+
+다음 시도 전략
+- datetime의 strptime, timedelta 메소드를 사용한다.
+- 경계값 처리 확실히 한다.
+- 슬라이딩 윈도우를 버리고, 모든 작업의 시작, 끝을 탐색돌면서 1초씩 더해 범위 안의 로그를 찾는다.
+"""
+
+# 성공한 시도
+import datetime
+
+def get_work_time(line):
+    # 날짜 / 시간 / 작업시간량
+    date, time, work_t = line.split(" ")
+    work_t = float(work_t[:-1]) - 0.001
+
+    finish = datetime.datetime.strptime(" ".join([date, time]), "%Y-%m-%d %H:%M:%S.%f")
+    start = finish - datetime.timedelta(seconds=work_t)
+
+    return [start, finish]
+
+
+def solution2(lines):
+    max_ = 0
+    work_times = []
+
+    for line in lines:
+        work_times.append(get_work_time(line))
+
+    for work_time in work_times:
+        for time in work_time:
+            count_ = 0
+
+            for log in work_times:
+                # 범위 끝(+0.999s)이 작업 시작시간보다 작은 경우
+                if time + datetime.timedelta(seconds=0.999) < log[0]:
+                    continue
+                # 범위 시작이 작업 끝시간보다 큰 경우
+                elif time > log[1]:
+                    continue
+                else:
+                    count_ += 1
+
+            if count_ > max_:
+                max_ = count_
+
+    return max_
+
+a = solution2(
+    [
+        "2016-09-15 20:59:57.421 0.351s",
+        "2016-09-15 20:59:58.233 1.181s",
+        "2016-09-15 20:59:58.299 0.8s",
+        "2016-09-15 20:59:58.688 1.041s",
+        "2016-09-15 20:59:59.591 1.412s",
+        "2016-09-15 21:00:00.464 1.466s",
+        "2016-09-15 21:00:00.741 1.581s",
+        "2016-09-15 21:00:00.748 2.31s",
+        "2016-09-15 21:00:00.966 0.381s",
+        "2016-09-15 21:00:02.066 2.62s"
+    ]
+)
+
+print(a)
