372b1dd333bff3ed107542140f5f18d69152d4ef
Kim Yoo <kimyoo04eco@naver.com>
2023-01-04 00:08:00 +0900

    Update 추석 트래픽, 2차원 리스트 순차 조회

--- 02_python-algorithm/01_concept/00_팁/2차원 리스트 순차 조회.py
+++ 02_python-algorithm/01_concept/00_팁/2차원 리스트 순차 조회.py
@@ -6,7 +6,16 @@ while i < 20:
     two_list.append([i, i+1])
     i += 2
 
+
 # two_list를 순차적으로 접근하는 이중 for 구문
 for one_list in two_list:
     for item in one_list:
         print(item)
+print()
+
+
+# two_list를 0인덱스부터 조회 후 1인덱스 순회
+for k in range(2):
+    for j in range(len(two_list)):
+        print(two_list[j][k])
+print()
